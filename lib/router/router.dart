b18f49702738c1c933ef66dfe70d4cdf00fe024e
LoTwT <709937065@qq.com>
2021-08-16 15:55:52 +0800

    category

--- lib/router/router.dart
+++ lib/router/router.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:music_player/views/home/home.dart';
 import 'package:music_player/views/splash/splash.dart';
+import 'package:music_player/views/square/square.dart';
 
 final routes = {
   "/": (BuildContext context) => Splash(),
   "/home": (BuildContext context) => Home(),
+  "/square": (BuildContext context) => Square(),
 };
--- lib/views/square/square.dart
+++ lib/views/square/square.dart
@@ -1,22 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:music_player/utils/screen_utils.dart';
-import 'package:music_player/views/home/category.dart';
-import 'package:music_player/views/home/header.dart';
-import 'package:music_player/views/home/swiper.dart';
 
-class Home extends StatelessWidget {
+class Square extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var screen = ScreenUtils(context);
+    var arguments = ModalRoute.of(context)!.settings.arguments as Map;
 
     return Scaffold(
       body: Container(
         padding: EdgeInsets.only(top: screen.screenPaddingTop + screen.calc(8)),
         child: Column(
           children: [
-            Header(),
-            Swiper(),
-            Category(),
+            Text("广场"),
+            Text(arguments["from"].toString()),
           ],
         ),
       ),
