dc5b089fb480f2e70d5f0f78f6e8d7f76bf650ef
LoTwT <709937065@qq.com>
2021-08-16 10:40:54 +0800

    splash

---
+++ lib/utils/screen_utils.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+// 设计图宽度
+const _width = 750;
+
+class ScreenUtils {
+  late MediaQueryData _screen;
+  // 屏幕宽度
+  late double _screenWidth;
+  // 屏幕高度
+  late double _screenHeight;
+  // 系统状态栏高度
+  late double _screenPaddingTop;
+
+  ScreenUtils(BuildContext context) {
+    _screen = MediaQuery.of(context);
+
+    _screenWidth = _screen.size.width;
+    _screenHeight = _screen.size.height;
+    _screenPaddingTop = _screen.padding.top;
+  }
+
+  double calc(double value) {
+    return value * _screenWidth / _width;
+  }
+}
--- lib/views/splash/splash.dart
+++ lib/views/splash/splash.dart
@@ -1,18 +1,34 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:music_player/utils/screen_utils.dart';
 
 class Splash extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Splash AppBar"),
+    final screen = ScreenUtils(context);
+    Timer(Duration(seconds: 2),
+        () => Navigator.pushReplacementNamed(context, "/home"));
+
+    return Container(
+      decoration: BoxDecoration(
+        color: Color(0xffc93e2d),
       ),
-      body: Row(
+      child: Stack(
         children: [
-          Text("Splash body"),
-          ElevatedButton(
-            onPressed: () => Navigator.pushReplacementNamed(context, "/home"),
-            child: Text("go home"),
+          Positioned(
+            child: Image.asset("assets/splash_logo.png"),
+            left: screen.calc(150),
+            top: screen.calc(300),
+            width: screen.calc(450),
+            height: screen.calc(89),
+          ),
+          Positioned(
+            child: Image.asset("assets/splash_logo_sm.png"),
+            left: screen.calc(264),
+            bottom: screen.calc(41),
+            width: screen.calc(222),
+            height: screen.calc(51),
           )
         ],
       ),
