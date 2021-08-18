7e4868b8f7a951d2b92af33edb497838b4b5ddfd
LoTwT <709937065@qq.com>
2021-08-18 14:26:56 +0800

    error fix

--- lib/utils/system_utils.dart
+++ lib/utils/system_utils.dart
@@ -4,13 +4,17 @@ import 'package:flutter/services.dart';
 
 /// 设置系统状态栏样式
 void setStatusBarStyle(Brightness iconstyle) {
-  if (Platform.isAndroid) {
-    SystemChrome.setSystemUIOverlayStyle(
-      SystemUiOverlayStyle(
-        // android 系统状态栏透明
-        statusBarColor: Colors.transparent,
-        statusBarIconBrightness: iconstyle,
-      ),
-    );
+  try {
+    if (Platform.isAndroid) {
+      SystemChrome.setSystemUIOverlayStyle(
+        SystemUiOverlayStyle(
+          // android 系统状态栏透明
+          statusBarColor: Colors.transparent,
+          statusBarIconBrightness: iconstyle,
+        ),
+      );
+    }
+  } catch (e) {
+    print(e);
   }
 }
